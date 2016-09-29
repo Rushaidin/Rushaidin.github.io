c2041e73bfcf8fb16766fe725d3281d73f36946d
Rushaidin <mrushaidin@gmail.com>
2016-09-29 16:58:18 +0800

    Add files via upload

---
+++ ToggleButton/js/main.js
@@ -0,0 +1,15 @@
+var main = function() {
+  
+  //var $skillset = $('.skillset');
+	//alert($skillset);
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  
+  $('.projects').hide();
+  $('.projects-button').on('click', function() {
+			$('.projects').toggle();
+      $('.projects-button').toggleClass('active');
+});
+}
+
+$(document).ready(main);
